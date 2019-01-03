c36861737bda4beecc5825ad1de862ecc7b158dd
saurav <sauravbhagat6969@gmail.com>
2019-01-03 19:45:10 +0530

    timer added

--- src/components/Landing/comingSoon.js
+++ src/components/Landing/comingSoon.js
@@ -1,22 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import renderHTML from 'react-render-html';
 
+import Logo from './../../images/rivieralogowhite.png';
 import './../css/comingSoon.css';
 
 class ComingSoon extends Component {
-	render() {
-		return (
-			<div className="cs-wrapper">
-				<div id='stars'></div>
-				<div id='stars2'></div>
-				<div id='stars3'></div>
+    constructor() {
+        super();
+        this.state = {
+            countdown: '',
+            target_date: ''
+        }
+    }
+    pad = (n)  => {
+        return (n < 10 ? '0' : '') + n;
+    }
+    getCountdown = () => {
+        let pad = this.pad;
+        let days, hours, minutes, seconds; // variables for time units
+        let countdown;
+        // find the amount of "seconds" between now and target
+        let current_date = new Date().getTime();
+        let seconds_left = (this.state.target_date - current_date) / 1000;
 
-				<div className="rv-text"><h1>Riviera 2019</h1></div>
-				<div className="cs-text-wrapper">
-					<div className="cs-text"><h1 class="glitch" data-text="Coming_Soon..">Coming_Soon..</h1></div>
-				</div>
-			</div>
-		)
-	}
+        days = pad(parseInt(seconds_left / 86400));
+        seconds_left = seconds_left % 86400;
+
+        hours = pad(parseInt(seconds_left / 3600));
+        seconds_left = seconds_left % 3600;
+
+        minutes = pad(parseInt(seconds_left / 60));
+        seconds = pad(parseInt(seconds_left % 60));
+
+        // format countdown string + set tag value
+        countdown = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span class='hide-in-phone'>" + seconds + "</span>";
+        this.setState({ countdown });
+    }
+    componentDidMount() {
+        let target_date = new Date().getTime() + (1000 * 3600 * 48); // set the countdown date
+        this.setState ({ target_date });
+        this.getCountdown();
+        setInterval( () => { this.getCountdown(); }, 1000);
+    }
+    render() {
+        return (
+            <div className="cs-wrapper">
+                <div id='stars'></div>
+                <div id='stars2'></div>
+                <div id='stars3'></div>
+
+                <div className="rv-text"><img src={Logo} alt="Logo" /></div>
+                <div className="cs-text-wrapper">
+                    <div className="cs-text">
+                        {/* <h1 class="glitch" data-text="Coming_Soon">Coming_Soon</h1> */}
+                        <br />
+                        <div className="social-icons">
+                            <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                            <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                            <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                        </div>
+                        <br />
+                        <div className="countdown">
+                            <div className='tiles'>{renderHTML(this.state.countdown)}</div>
+                            <div class="labels">
+                                <li>Days</li>
+                                <li>Hours</li>
+                                <li>Mins</li>
+                                <li className="hide-in-phone">Secs</li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default ComingSoon;
