4d2cd05939bf902261f61fec2b7e5c619c5f72e0
adityasrivast <yashsri421@gmail.com>
2019-01-05 12:56:48 +0530

    Updates

---
+++ precache-manifest.170f7618edf25ada5e132cc50d5ea27c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd5e28914c8fe20a7e0b17c4ba7d8b29",
+    "url": "/static/media/rivieralogowhite.dd5e2891.png"
+  },
+  {
+    "revision": "c418bd1e7fec481e5138",
+    "url": "/static/css/main.14105ef0.chunk.css"
+  },
+  {
+    "revision": "5f12caa52f0151391915c7177a146127",
+    "url": "/static/media/back2.5f12caa5.jpg"
+  },
+  {
+    "revision": "a4143a20abacda34d8c0",
+    "url": "/static/js/1.a4143a20.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78407c67f5db5476a8ac022230a71db4",
+    "url": "/static/media/events.78407c67.jpg"
+  },
+  {
+    "revision": "3927447f3ce2401d3c8e7db52e26b840",
+    "url": "/static/media/rivieralogo.3927447f.png"
+  },
+  {
+    "revision": "383ae734a87aba036f9c05cb8cdaa0e5",
+    "url": "/static/media/maruti.383ae734.jpg"
+  },
+  {
+    "revision": "e223a418a787df283b4595551c855386",
+    "url": "/static/media/pepsi.e223a418.png"
+  },
+  {
+    "revision": "ebf5b3ae03e69f5ff458fbe0da7231bf",
+    "url": "/static/media/about.ebf5b3ae.png"
+  },
+  {
+    "revision": "c418bd1e7fec481e5138",
+    "url": "/static/js/main.c418bd1e.chunk.js"
+  },
+  {
+    "revision": "b532beab8ca887878674ee6c01326fab",
+    "url": "/static/media/events.b532beab.png"
+  },
+  {
+    "revision": "18ed58dcbe62ea56b6e6acf99dfef8dc",
+    "url": "/static/media/sports.18ed58dc.png"
+  },
+  {
+    "revision": "31161734b5f0ae7a9dd9a0e08b20b513",
+    "url": "/static/media/proshows.31161734.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a6731766277b0538d96d20995513f13f",
+    "url": "/static/media/comingsoon.a6731766.jpg"
+  },
+  {
+    "revision": "a4143a20abacda34d8c0",
+    "url": "/static/css/1.f619bcea.chunk.css"
+  },
+  {
+    "revision": "db5d3694da4950458255033bb00e30d1",
+    "url": "/index.html"
+  }
+];
