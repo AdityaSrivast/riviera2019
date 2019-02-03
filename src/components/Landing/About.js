e63b7544a89abbffb2cfea09761cfe06c78bcaae
Manmeet Tarun <meetutarun16@gmail.com>
2019-02-03 22:38:16 +0530

    Changed scrollbar

--- src/components/Landing/About.js
+++ src/components/Landing/About.js
@@ -13,11 +13,23 @@ class About extends Component {
                 <div className="about-content">
                     <div>
                         <p>
-                        VIT Vellore is proud to present Riviera, a cultural extravagance!
-The birth of Riviera was in the year 2000 with an idea to create something big, and we have stuck to that idea and grown exponentially ever since then. This sports and cultural fest is an ISO 9001-2015 certified event and is considered one of the best college festival in the country. Riviera witnesses over 40,000 students from 700 colleges from all over India and across the globe. 
-From Comedy nights to Gaming Tournaments. From Quizzes to Marathons. From Literary competitions to Sporting leagues. Riviera has it all!
-This 4-day long fest is not just about fun but also about creating the next generation of extraordinary students who not just excel in academics but also sports and culturals.
+                            VIT Vellore is proud to present Riviera, a cultural extravagance!
+                            The birth of Riviera was in the year 2000 with an idea to create something big, and we have stuck to that idea and grown exponentially ever since then. This sports and cultural fest is an ISO 9001-2015 certified event and is considered one of the best college festival in the country. Riviera witnesses over 40,000 students from 700 colleges from all over India and across the globe. 
+                            From Comedy nights to Gaming Tournaments. From Quizzes to Marathons. From Literary competitions to Sporting leagues. Riviera has it all!
+                            This 4-day long fest is not just about fun but also about creating the next generation of extraordinary students who not just excel in academics but also sports and culturals.
 						</p>
+                    </div>
+                </div>
+
+
+                <div className="spon-heading">
+                    <h1 className="heading">Theme</h1>
+                </div>
+                <div className="about-content">
+                    <div>
+                        <p>
+                            Ever realized how there is a certain sequence to the things you do on daily basis? A subconscious natural rhythm takes over your body as soon as you wake up in the morning. Eat, work, repeat. Monotony is when you let your routine dictate your schedule, your choices and your life. So, Take charge, do something thrilling and electrifying, come <b>Shatter the monotony</b>  at Riviera '19
+                        </p>
                     </div>
                 </div>
             </div>
